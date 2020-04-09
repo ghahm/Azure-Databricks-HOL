64ea4433e1c393a207529965ed40d7cb67194bfb
Gwangseok Hahm <ghahm@hotmail.com>
2020-04-10 00:57:14 +0900

    Update: README.md notebooks

---
+++ notebooks/Users/ghahm@microsoft.com/Lab12-Git-versioning.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %md ##### GitHub version control- https://docs.databricks.com/notebooks/github-version-control.html#github-version-control
+
+# COMMAND ----------
+
+# Get an access token -  https://docs.databricks.com/notebooks/github-version-control.html#get-an-access-token
