906716b7cb231a25655d9c7bb2a1b94f6092cd06
Gwangseok Hahm <ghahm@hotmail.com>
2022-12-08 18:14:40 +0900

    Update Lab02-03-Advanced-RDD-Programming.py

--- notebooks/Users/ghahm@microsoft.com/Lab02-03-Advanced-RDD-Programming.py
+++ notebooks/Users/ghahm@microsoft.com/Lab02-03-Advanced-RDD-Programming.py
@@ -4,7 +4,8 @@
 # COMMAND ----------
 
 # 3.1 RDD 생성하고 자료 조회하기
-flightRdd = sc.textFile("/mnt/demodata/sparkhol/flight-data/flights.csv").map(lambda line: line.split(",")) 
+#flightRdd = sc.textFile("/mnt/demodata/sparkhol/flight-data/flights.csv").map(lambda line: line.split(",")) 
+flightRdd = sc.textFile("abfss://demodata@ghadlskrc.dfs.core.windows.net/sparkhol/flight-data/flights.csv").map(lambda line: line.split(",")) 
 #flightRdd.take(1)
 flightRdd.count()
 
