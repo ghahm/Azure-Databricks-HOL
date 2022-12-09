d37220fa164ccf81919ad6649ff9b1ed95a30ad3
Gwangseok Hahm <ghahm@hotmail.com>
2022-12-09 12:20:14 +0900

    Update Lab02-04-Advanced-RDD-Programming.py

--- notebooks/Users/ghahm@microsoft.com/Lab02-04-Advanced-RDD-Programming.py
+++ notebooks/Users/ghahm@microsoft.com/Lab02-04-Advanced-RDD-Programming.py
@@ -11,7 +11,8 @@ ordersRdd1.count()
 # COMMAND ----------
 
 # Spark Web UI의 Stage 메뉴에서 task 개수를 참고하여 파티션 수 확인 -> 총 파일 수 : 1.6 MB 파일 2개  --> RDD 파티션 2개
-ordersRdd2=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/small-[1-2]/orders.tbl").map(lambda line: line.split("|")) 
+#ordersRdd2=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/small-[1-2]/orders.tbl").map(lambda line: line.split("|")) 
+ordersRdd2=spark.sparkContext.textFile("abfss://demodata@ghadlskrc.dfs.core.windows.net/sparkhol/rdd-partition-data/ordersTbls/small-[1-2]/orders.tbl").map(lambda line: line.split("|")) 
 ordersRdd2.getNumPartitions()
 
 # COMMAND ----------
@@ -21,7 +22,8 @@ ordersRdd2.count()
 # COMMAND ----------
 
 # Spark Web UI의 Stage 메뉴에서 task 개수를 참고하여 파티션 수 확인 -> 총 파일 수 : 1.6 MB 파일 3개  --> RDD 파티션 3개
-ordersRdd3=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/small-[1-3]/orders.tbl").map(lambda line: line.split("|")) 
+#ordersRdd3=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/small-[1-3]/orders.tbl").map(lambda line: line.split("|")) 
+ordersRdd3=spark.sparkContext.textFile("abfss://demodata@ghadlskrc.dfs.core.windows.net/sparkhol/rdd-partition-data/ordersTbls/small-[1-3]/orders.tbl").map(lambda line: line.split("|")) 
 ordersRdd3.getNumPartitions()
 
 # COMMAND ----------
@@ -32,7 +34,8 @@ ordersRdd3.count()
 
 # Spark Web UI의 Stage 메뉴에서 task 개수를 참고하여 파티션 수 확인 -> 총 파일 수 : 1.6 MB 파일 7개 & 164 MB 1개 --> RDD 파티션 9개 (1.6 MB 7개와 128 MB + 32MB 2개로 나뉘어짐) 
 # (실제 Spark Web UI의 Tasks 별 Input Size를 보면 128MB와는 약간의 차이가 있을 수 있음)
-ordersRdd4=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/*/orders.tbl").map(lambda line: line.split("|")) 
+#ordersRdd4=spark.sparkContext.textFile("/mnt/demodata/sparkhol/rdd-partition-data/ordersTbls/*/orders.tbl").map(lambda line: line.split("|")) 
+ordersRdd4=spark.sparkContext.textFile("abfss://demodata@ghadlskrc.dfs.core.windows.net/sparkhol/rdd-partition-data/ordersTbls/*/orders.tbl").map(lambda line: line.split("|")) 
 ordersRdd4.getNumPartitions()
 
 # COMMAND ----------
