62622a03b91feacaefd664664482b9fd11d7a8b9
Gwangseok Hahm <ghahm@hotmail.com>
2022-12-09 13:46:58 +0900

    Update Lab10-create-delta-table.py

--- notebooks/Users/ghahm@microsoft.com/Lab10-create-delta-table.py
+++ notebooks/Users/ghahm@microsoft.com/Lab10-create-delta-table.py
@@ -4,16 +4,19 @@
 # COMMAND ----------
 
 # 1. 소스 데이터 파일을 읽어 Dataframe 생성하고 자료 조회하기
-usersDF = spark.read.option("multiline", "true").json("/mnt/demodata/sparkhol/json-data/all-users.json.gz")
+#usersDF = spark.read.option("multiline", "true").json("/mnt/demodata/sparkhol/json-data/all-users.json.gz")
+usersDF = spark.read.option("multiline", "true").json("abfss://demodata@ghadlskrc.dfs.core.windows.net/sparkhol/json-data/all-users.json.gz")
 #usersDF.printSchema()
 usersDF.show()
 
 # COMMAND ----------
 
 # 2. Delta 테이블 생성하기 --> overwrite / append
-usersDF.write.format("delta").mode("overwrite").save("/mnt/demodata/delta/users") # Managed Overwrite
-#usersDF.write.format("delta").mode("append").save("/mnt/demodata/delta/users") # Managed Overwrite
-spark.sql("CREATE TABLE delta_df_users USING DELTA LOCATION '/mnt/demodata/delta/users'")
+usersDF.write.format("delta").mode("overwrite").save("abfss://demodata@ghadlskrc.dfs.core.windows.net/delta/users") # Managed Overwrite
+#usersDF.write.format("delta").mode("append").save("abfss://demodata@ghadlskrc.dfs.core.windows.net/delta/users") # Managed Overwrite
+#usersDF.write.format("delta").mode("overwrite").save("abfss://demodata@ghadlskrc.dfs.core.windows.net/delta/users") # Managed Overwrite
+#usersDF.write.format("delta").mode("append").save("abfss://demodata@ghadlskrc.dfs.core.windows.net/delta/users") # Managed Overwrite
+spark.sql("CREATE TABLE delta_df_users USING DELTA LOCATION 'abfss://demodata@ghadlskrc.dfs.core.windows.net/delta/users'")
 
 # COMMAND ----------
 
